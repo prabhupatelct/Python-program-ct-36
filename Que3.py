bca4e68ac20d68d166e4b2ea4237ac8eae0baf54
Prabhu Patel <81961978+prabhupatelct@users.noreply.github.com>
2021-10-10 22:37:11 +0530

    Update and rename Question2.py to Que3.py

---
+++ Que3.py
@@ -0,0 +1,3 @@
+string = '''Every ending is a new  beginning.'''
+double_spaces = string.find("  ")
+print(double_spaces)
--- Question2.py
+++
@@ -1,9 +0,0 @@
-letter=''' Dear <|name|>
-You are selected
-<|date|>'''
-
-name= str(input("Enter your name= \n" ))
-date= (input("Enter Date= \n"))
-letter=letter.replace("<|name|>", name)
-letter=letter.replace("<|date|>", date)
-print(letter)
