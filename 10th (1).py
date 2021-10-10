226cce3afd593f0a9a60a739291d093af564d5f4
Prabhu Patel <81961978+prabhupatelct@users.noreply.github.com>
2021-10-10 22:26:16 +0530

    Add files via upload

---
+++ 10th (1).py
@@ -0,0 +1,14 @@
+myDict = {
+
+    "diwar" : "wall", 
+    "khidki" : "window",
+    "bura" : "bad",
+    "dodh" : "milk",
+    "chai" : "tea",
+}
+
+print("Options are : ", myDict.keys())
+
+a = input("Enter Your Word : ")
+
+print("The meaning of your word is : ", myDict[a]) 
---
+++ 9th (1).py
@@ -0,0 +1,6 @@
+t = (1,0,2,5,0,0,6)
+c = 0 
+for x in t: 
+    if x == 0: 
+        c += 1 
+print("Number of zeros : " , c) 
